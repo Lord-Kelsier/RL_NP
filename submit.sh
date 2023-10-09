edc3e5cec42bfa3e445db25e0ffe0cb2625cfd28
Eduardo Contreras <econtreraslazcano@uc.cl>
2023-10-09 00:07:24 -0300

    fix partition name

--- submit.sh
+++ submit.sh
@@ -12,7 +12,7 @@
 ##SBATCH --mail-user=econtreraslazcano@uc.cl
 ##SBATCH --mail-type=ALL
 ## USANDO GPU:
-#SBATCH --partition=gpu
+#SBATCH --partition=gpus
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
