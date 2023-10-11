a82f70362a78781fbbcda31f6e9ef14cd07c7df5
Eduardo Contreras <econtreraslazcano@uc.cl>
2023-10-10 23:44:38 -0300

    changed names

--- submit.sh
+++ submit.sh
@@ -3,7 +3,7 @@
 ## Nombre del trabajo
 #SBATCH --job-name=TestRLNPRandomPolicy
 ## Archivo de salida
-#SBATCH --output=../ExpResults/salida.txt
+#SBATCH --output=../ExpResults/salida-noRandom.txt
 ### Partición (Cola de trabajo)
 ##SBATCH --partition=512x1024
 ## Solicitud de cpus
@@ -12,4 +12,4 @@
 #SBATCH --mail-user=econtreraslazcano@uc.cl
 #SBATCH --mail-type=ALL
 
-python scripts/run.py --num_steps_per_iter=8192 --vf_coef=0.1 --model=internal --ex_dir=../ExpResults --name=NoTimeInternal1e7StepsRandomPolicy --actor_network_width=64 --critic_network_width=256 --actor_depth=1 --optimizer=adam --learning_rate=0.0003 --entropy_coef=0.03 --target_kl=0.005 --min_mean_distance=2.4 --max_mean_distance=3.1 --min_atomic_distance=2.4 --formulas=Au13 --canvas_size=13 --save_rollouts=eval --clip_ratio=0.25 --lam=1.0 --discount=1.0 --num_steps=10000000 --eval_freq=50
+python scripts/run.py --num_steps_per_iter=8192 --vf_coef=0.1 --model=internal --ex_dir=../ExpResults --name=NoTimeInternal1e7Steps --actor_network_width=64 --critic_network_width=256 --actor_depth=1 --optimizer=adam --learning_rate=0.0003 --entropy_coef=0.03 --target_kl=0.005 --min_mean_distance=2.4 --max_mean_distance=3.1 --min_atomic_distance=2.4 --formulas=Au13 --canvas_size=13 --save_rollouts=eval --clip_ratio=0.25 --lam=1.0 --discount=1.0 --num_steps=10000000 --eval_freq=50
