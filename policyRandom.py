af89a5ade52ae5beb29dc217ee058a19ee31ecc1
Eduardo Contreras <econtreraslazcano@uc.cl>
2023-11-12 02:44:42 -0300

    set main to original and fixed params

--- policyRandom.py
+++ policyRandom.py
@@ -7,9 +7,9 @@ def randomPolicy(observation):
   stop = 0.0
   focus = np.random.choice([e for e in range(i )])
   element = 1.0
-  distance = np.random.normal(2.75, 0.8)
+  distance = np.random.normal(2.75, 0.5)
   angle = np.random.uniform(0.0, np.pi)
-  dihedral = np.random.uniform(-np.pi, np.pi)
+  dihedral = np.random.uniform(0, np.pi)
   kappa = np.random.choice([-1, 1])
   out =  np.array([stop, focus, element, distance, angle, dihedral, kappa])
   return out
