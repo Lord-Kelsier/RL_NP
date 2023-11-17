7cfeb55724b9b7c880839d43628e6984169dd90c
Eduardo Contreras <econtreraslazcano@uc.cl>
2023-11-16 22:15:38 -0300

    Al6Cu7 bi-metalic cluster

--- submit.sh
+++ submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ## Nombre del trabajo
-#SBATCH --job-name=OrigAu7Ag6
+#SBATCH --job-name=OrigAl6Cu7
 ## Archivo de salida
-#SBATCH --output=../ExpResults/originalAu7Ag6.txt
+#SBATCH --output=../ExpResults/originalAl6Cu7.txt
 ### Partición (Cola de trabajo)
 ##SBATCH --partition=512x1024
 ## Solicitud de cpus
@@ -13,4 +13,4 @@
 #SBATCH --mail-user=econtreraslazcano@uc.cl
 #SBATCH --mail-type=ALL
 
-python scripts/run.py --num_steps_per_iter=8192 --vf_coef=0.1 --model=internal --ex_dir=../ExpResults --name=OriginalAu7Ag6 --actor_network_width=64 --critic_network_width=256 --actor_depth=1 --optimizer=adam --learning_rate=0.0003 --entropy_coef=0.03 --target_kl=0.005 --min_mean_distance=2.4 --max_mean_distance=3.1 --min_atomic_distance=2.4 --formulas=Au7Ag6 --canvas_size=13 --save_rollouts=eval --clip_ratio=0.25 --lam=1.0 --discount=1.0 --num_steps=10000000000 --eval_freq=50
+python scripts/run.py --num_steps_per_iter=8192 --vf_coef=0.1 --model=internal --ex_dir=../ExpResults --name=OriginalAl6Cu7 --actor_network_width=64 --critic_network_width=256 --actor_depth=1 --optimizer=adam --learning_rate=0.0003 --entropy_coef=0.03 --target_kl=0.005 --min_mean_distance=2.4 --max_mean_distance=3.1 --min_atomic_distance=2.4 --formulas=Al6Cu7 --canvas_size=13 --save_rollouts=eval --clip_ratio=0.25 --lam=1.0 --discount=1.0 --num_steps=10000000000 --eval_freq=50
